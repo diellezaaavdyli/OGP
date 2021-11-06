9c04acde4f66b6a9446d41092f5dddb15ec3a379
GranitSalihu <89086751+GranitSalihu@users.noreply.github.com>
2021-11-06 21:14:27 +0100

    multilanguage,in one page

---
+++ js/languages.js
@@ -0,0 +1,114 @@
+
+    var arrLang = {
+      'en': {
+        'home': 'Home',
+        'about': 'About Us',
+        'documents': 'Documents',
+        'team' :'Member',
+        'contact':'Contact',
+        'Open':'Open Government Partnership Kosova Initiative.',
+        'Check':'Check the OGP Process Milestones',
+        'Aligibility ':'Eligibility Criteria and Pass the Values Check',
+        'Ministry':'Ministry of Local Government Administration',
+        'Submitting':'Submitting the Letter of Intent',
+        'Engaging':'Engaging Civil Society Organizations',
+        'Submittingg':'Submitting the National Action Plan.',
+        'ABOUT':'ABOUT', 
+        'Kosovo':'The Kosovo government in its meeting on 9 July 2013 took ​​a decision to initiate the procedures for membership of the Republic of Kosovo in the multinational initiative',
+        'Kosovo1':'The decision obliged the Government of Kosovo to implement all obligations required under the OGP membership.As such, the Government of Kosovo with the appointed lead Ministry of European Integration submitted all the necessary documents back in 2013 to become part of the initiative.',
+        'Unfortunately':'Unfortunately, the Kosovo government received the notification letter that it cannot be part of the initiative. Ever since, the Republic of Kosovo has committed to the principles of open and accountable government, and continued its attempts to put Kosovo in the digital governance realm.',
+        'With':'With the adoption of the National Open Data Charter, the E-Kosova state portal, public consultation platforms, public service digitalisation, e-participation platform, state budget transparency tools and many more, the Kosovo government believes it now possesses the necessary principles to be part of the OGP. On 8 June 2021 the government of Kosovo once again took the decision to begin the initiative for membership at the OGP.',
+        'The' :'The lead Ministry appointed to coordinate and foster the national initiative is the Ministry of Local Government Administration. The National Action Plan will cover the period of 2022-2024 and will provide concrete feasible action items that aim at increasing governmental transparency, all while including the civil society organizations as main catalysts of the initiative.',
+        'This' :'This website provides all important documents and other relevant information regarding the Republic of Kosovo and its second attempt to become part of the OGP. Everyone is encouraged to provide their comments regarding the initiative so that the process is transparent and reflects the opinions of the citizens of the Republic of Kosovo.',
+        'IMPORTANT ':'IMPORTANT DOCUMENTS',
+        'ogp' :'OGP Kosova Budget',
+        'Letter':'Letter of Intent',
+        'OGD':'Open Government Declaration',
+        'nap':'National Action Plan',
+        'DGK':'Decisions from theGovernment of Kosovo',
+        'moduls':'There are no attachments yet uploaded by the admin panel for this box, please try again in the following period.',
+        'NATIONAL':'OGP NATIONAL COORDINATING COMMITTEE',
+        'Ex':'Ex Rilindja building Floors: 11, 12 and 13 Pristina, Republic of Kosovo',
+        'phone':'Phone',
+        'email':'Email',
+        'Copy':'© Copyright 2021 Open Data Kosovo. All Rights Reserved.'
+      },
+      'sq': {
+        'home': 'Faqja Kryesore',
+        'about': 'Rreth Nesh',
+        'documents':'Documentet',
+        'team':'Anëtar',
+        'contact':'Kontakti',
+        'Open':'Iniciativa për Partneritet të Hapur të Qeverisë të Kosovës .',
+        'Check':'Kontrolloni arritjet e procesit OGP',
+        'Aligibility ':'Pranueshmëria e Kritereve dhe Kontrolli i Vlerave Kalimtare',
+        'Ministry':'Ministria e Administrimit të Pushtetit Lokal',
+        'Submitting':'Dorëzimi i Letrës të Rekomandimit',
+        'Engaging':'Angazhimi i Organizatave të Shoqërisë Civile',
+        'Submittingg':'Dorëzimi i Planit Kombëtar të Veprimit.',
+        'ABOUT':'RRETH PLATFORMËS',
+        'Kosovo':'Qeveria e Kosovës në mbledhjen e saj më 9 korrik 2013 mori një vendim për fillimin e procedurave për anëtarësimin e Republikës së Kosovës në nismën shumëkombëshe ',
+        'Kosovo1':'Vendimi e obligoi Qeverinë e Kosovës të zbatojë të gjitha obligimet e kërkuara për anëtarësim në OGP. Si e tillë, Qeveria e Kosovës me Ministrinë udhëheqëse të emëruar të Integrimit Evropian dorëzoi të gjitha dokumentet e nevojshme në vitin 2013 për tu bërë pjesë e nismës.',
+        'Unfortunately':'Fatkeqësisht, qeveria e Kosovës pranoi letrën e njoftimit se nuk mund të jetë pjesë e nismës. Që atëherë, Republika e Kosovës është përkushtuar ndaj parimeve të qeverisjes së hapur dhe të përgjegjshme, dhe ka vazhduar përpjekjet e saj për ta vënë Kosovën në sferën e qeverisjes dixhitale.',
+        'With':'Me miratimin e Kartës Kombëtare të të Dhënave të Hapura, portalit shtetëror E-Kosova, platformave të konsultimit publik, dixhitalizimit të shërbimeve publike, platformës së pjesëmarrjes elektronike, mjeteve të transparencës së buxhetit të shtetit dhe shumë më tepër, qeveria e Kosovës beson se tani posedon parimet e nevojshme për të qenë pjesë e PQH-së. Më 8 qershor 2021 qeveria e Kosovës edhe një herë mori vendimin për të filluar iniciativën për anëtarësim në OGP.',
+        'The' :'Ministria udhëheqëse e emëruar për të koordinuar dhe nxitur nismën kombëtare është Ministria e Administrimit të Pushtetit Lokal. Plani Kombëtar i Veprimit do të mbulojë periudhën 2022-2024 dhe do të sigurojë artikuj konkretë të mundshëm veprimi që synojnë rritjen e transparencës qeveritare, duke përfshirë të gjitha organizatat e shoqërisë civile si katalizatorët kryesorë të nismës.',
+        'This' :'Kjo faqe e internetit ofron të gjitha dokumentet e rëndësishme dhe informata të tjera përkatëse në lidhje me Republikën e Kosovës dhe përpjekjen e saj të dytë për tu bërë pjesë e PQH-së. Të gjithë inkurajohen të japin komentet e tyre lidhur me nismën në mënyrë që procesi të jetë transparent dhe të pasqyrojë opinionet e qytetarëve të Republikës së Kosovës.',
+        'IMPORTANT':'DOKUMENTET E RËNDËSISHME',
+        'ogp' :'OGP Buxheti i Kosovës',
+        'Letter':'Letër Rekomandimi',
+        'OGD':'Deklarata e Qeverisë së Hapur',
+        'nap':'Plani Kombëtar i Veprimit',
+        'DGK':'Vendimet e Qeverisë së Kosovës',
+        'moduls':'Nuk ka të bashkëngjitur apo të ngarkuar asgjë nga ana e panelit administratues për këtë kuti, ju lutemi provoni përsëri në periudhën vijuese.',
+        'NATIONAL':'OGP NATIONAL COORDINATING COMMITTEE',
+        'Ex':'Ndërtesa e Ish Rilindjes, Katet: 11, 12 dhe 13, Prishtinë, Republika e Kosovës',
+        'phone':'Telefoni',
+        'email':'Email',
+        'Copy':'E drejta e autorit Open Data Kosovos. Të gjitha të drejtat e rezervuara'
+      },
+      'srb' :{
+          'home':'Glavn stranica',
+          'about':'O platformi',
+          'documents':'Dokumenti',
+          'team':'Član',
+          'contact':'Kontakt',
+          'Open':'Inicijativa za Otvoreno Vlade Kosova .',
+          'Check':'Proverite prekretnice OGP procesa',
+          'Aligibility':'Kriterijumi Podobnosti i Kontrola Prolazne Vrednosti',
+          'Ministry':'Ministarstvo Administracije Lokalne Samouprave',
+          'Submitting':'Podnošenje Preporučnog Pisma',
+          'Engaging':'Angažovanje Organizacija Civilnog Društva',
+          'Submittingg':'Podnošenje Nacionalnog Akcionog Plana.',
+          'ABOUT':'O PLATFORMI',
+          'Kosovo':'Vlada Kosova je na svom sastanku 9. jula 2013. godine donela odluku o okončanju procedura za članstvo Republike Kosovo u multinacionalnom pokretu ',
+          'Kosovo1':' Odluka je obavezala Vladu Kosova da sprovede sve obaveze koje su potrebne prema članstvu u OPV -u. Kao takav, Vlada Kosova sa imenovanim Ministarstvom za Evropske Integracije od 2013. godine.',
+          'Unfortunately':'Nažalost, vlada Kosova primila je pismo obaveštenja da ne može biti deo inicijative. Od tada je Republika Kosovo usvojila principe otvorenog i odgovornog pristupa i da se Kosovo stavi u oblast digitalnog upravljanja.',
+          'With':'Usvajanjem Nacionalne Povelje o otvorenim podacima, državnog portala E-Kosovo, platformi za javne konsultacije, digitalizacije javnih usluga, platforme za e-učešće, sredstava za transparentnost državnog budžeta i mnogih drugih, vlada Kosova veruje da sada poseduje neophodne principe deo OGP -a. Vlada Kosova je 8. juna 2021. godine ponovo donela odluku o pokretanju inicijative za članstvo u OPV.',
+          'The':'Vodeće Ministarstvo imenovan za koordinaciju i podsticanje nacionalne inicijative je Ministarstvo Lokalne Uprave. Nacionalni Akcioni Plan pokrivaće period od 2022. do 2024. godine i daće konkretne izvodljive stavke delovanja koje imaju za cilj povećanje transparentnosti vlade, a sve organizacije civilnog društva uključuju kao glavne katalizatore inicijative.',
+          'This':'Ova veb stranica pruža sve važne dokumente i druge relevantne informacije o Republici Kosovo i njenom drugom pokušaju da postane deo OPV -a. Pozivaju se svi da daju svoje komentare u vezi sa inicijativom kako bi proces bio transparentan i odražavao mišljenja građana Republike Kosovo.',
+          'IMPORTANT':'VAŽNI DOKUMENTI',
+          'ogp':'OGP Kosovski Budžet',
+          'Letter':'Pismo Preporuke',
+          'OGD':'Otvorena Deklaracija o Vladi',
+          'nap':'Nacionalni Akcioni Plan',
+          'DGK':'Odluke Vlade Kosova',
+          'moduls':'Nema ničega priloženo ili otpremljeno za ovu kutiju od strane administratorskog panela, pokušajte ponovo kasnije.',
+          'NATIONAL':'OGP NATIONAL COORDINATING COMMITTEE',
+          'Ex':'Bivša zgrada Rilindja, Spratovi: 11, 12 i 13, Priština, Republika Kosovo',
+          'phone':'Telefon',
+          'email':'Email:',
+          'Copy':'©Autorsko Pravo Open Data Kosovos. Sva Prava Zadržana'
+      }
+    };
+
+    // Process translation
+    $(function() {
+      $('.translate').click(function() {
+        var lang = $(this).attr('id');
+
+        $('.lang').each(function(index, item) {
+          $(this).text(arrLang[lang][$(this).attr('key')]);
+        });
+      });
+    });
+
